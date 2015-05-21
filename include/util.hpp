e3f4aa58eccbace37648d876907fef071ab6adae
Jon Ross <jonross.zlsa@gmail.com>
2015-05-21 12:49:35 -0700

    second commit, now there is actually a "hello world" file

---
+++ include/util.hpp
@@ -0,0 +1,7 @@
+
+#ifndef UTIL_H
+#define UTIL_H
+
+#define NEWLINE "\n"
+
+#endif
---
+++ main.cpp
@@ -0,0 +1,10 @@
+
+#include <iostream>
+
+#include "pflight_config.hpp"
+#include "util.hpp"
+
+int main(int argc, char *argv[]) {
+  std::cout << "pFlight version " << PFLIGHT_VERSION << "." << NEWLINE;
+  return(0);
+}
